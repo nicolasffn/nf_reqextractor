6087aa9d4678a9db90646870445efcad0ae20293
httpsm9k <nclas91@gmail.com>
2023-02-23 09:16:40 +0100

    v1.0.0 Updated

--- v1.0.0/app.py
+++ v1.0.0/app.py
@@ -14,10 +14,10 @@ def predict():
     req_text = request.form['input_text']
     result = predict_requirement(req_text)
     labels_list = result[0]
-    confidence_list = [(round(float(c),4)*100) for c in result[1]]
+    confidence_list = [str(round(float(c) * 100, 2)) for c in result[1]]
 
     # Add the current prediction and input text to the history
-    history.append((req_text, labels_list, confidence_list))
+    history.insert(0, (req_text, labels_list, confidence_list))
 
     # Pass the prediction results and history to the HTML template
     return render_template('index.html', results=list(zip(labels_list, confidence_list)), history=history)
